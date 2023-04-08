f2d5305029436969516928d3c67b23de5b51af9e
Matteo Capitanio <matteo.capitanio@gmail.com>
2023-04-08 22:13:29 +0200

    update

--- finetune.py
+++ finetune.py
@@ -23,49 +23,49 @@ from peft import (
 
 
 def train(
-    # model/data params
-    base_model: str = "",  # the only required argument
-    data_path: str = "./alpaca_data_cleaned.json",
-    output_dir: str = "./lora-alpaca",
-    # training hyperparams
-    batch_size: int = 128,
-    micro_batch_size: int = 4,
-    num_epochs: int = 3,
-    learning_rate: float = 3e-4,
-    cutoff_len: int = 256,
-    val_set_size: int = 2000,
-    # lora hyperparams
-    lora_r: int = 8,
-    lora_alpha: int = 16,
-    lora_dropout: float = 0.05,
-    lora_target_modules: List[str] = [
-        "q_proj",
-        "v_proj",
-    ],
-    # llm hyperparams
-    train_on_inputs: bool = True,  # if False, masks out inputs in loss
-    group_by_length: bool = False,  # faster, but produces an odd training loss curve,
-    resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
+        # model/data params
+        base_model: str = "",  # the only required argument
+        data_path: str = "./alpaca_data_cleaned.json",
+        output_dir: str = "./lora-alpaca",
+        # training hyperparams
+        batch_size: int = 128,
+        micro_batch_size: int = 4,
+        num_epochs: int = 3,
+        learning_rate: float = 3e-4,
+        cutoff_len: int = 256,
+        val_set_size: int = 2000,
+        # lora hyperparams
+        lora_r: int = 8,
+        lora_alpha: int = 16,
+        lora_dropout: float = 0.05,
+        lora_target_modules: List[str] = [
+            "q_proj",
+            "v_proj",
+        ],
+        # llm hyperparams
+        train_on_inputs: bool = True,  # if False, masks out inputs in loss
+        group_by_length:
+    bool = False,  # faster, but produces an odd training loss curve,
+        resume_from_checkpoint:
+    str = None,  # either training checkpoint or final adapter
 ):
-    print(
-        f"Training Alpaca-LoRA model with params:\n"
-        f"base_model: {base_model}\n"
-        f"data_path: {data_path}\n"
-        f"output_dir: {output_dir}\n"
-        f"batch_size: {batch_size}\n"
-        f"micro_batch_size: {micro_batch_size}\n"
-        f"num_epochs: {num_epochs}\n"
-        f"learning_rate: {learning_rate}\n"
-        f"cutoff_len: {cutoff_len}\n"
-        f"val_set_size: {val_set_size}\n"
-        f"lora_r: {lora_r}\n"
-        f"lora_alpha: {lora_alpha}\n"
-        f"lora_dropout: {lora_dropout}\n"
-        f"lora_target_modules: {lora_target_modules}\n"
-        f"train_on_inputs: {train_on_inputs}\n"
-        f"group_by_length: {group_by_length}\n"
-        f"resume_from_checkpoint: {resume_from_checkpoint}\n"
-    )
+    print(f"Training Alpaca-LoRA model with params:\n"
+          f"base_model: {base_model}\n"
+          f"data_path: {data_path}\n"
+          f"output_dir: {output_dir}\n"
+          f"batch_size: {batch_size}\n"
+          f"micro_batch_size: {micro_batch_size}\n"
+          f"num_epochs: {num_epochs}\n"
+          f"learning_rate: {learning_rate}\n"
+          f"cutoff_len: {cutoff_len}\n"
+          f"val_set_size: {val_set_size}\n"
+          f"lora_r: {lora_r}\n"
+          f"lora_alpha: {lora_alpha}\n"
+          f"lora_dropout: {lora_dropout}\n"
+          f"lora_target_modules: {lora_target_modules}\n"
+          f"train_on_inputs: {train_on_inputs}\n"
+          f"group_by_length: {group_by_length}\n"
+          f"resume_from_checkpoint: {resume_from_checkpoint}\n")
     assert (
         base_model
     ), "Please specify a --base_model, e.g. --base_model='decapoda-research/llama-7b-hf'"
@@ -100,11 +100,8 @@ def train(
             padding=False,
             return_tensors=None,
         )
-        if (
-            result["input_ids"][-1] != tokenizer.eos_token_id
-            and len(result["input_ids"]) < cutoff_len
-            and add_eos_token
-        ):
+        if (result["input_ids"][-1] != tokenizer.eos_token_id
+                and len(result["input_ids"]) < cutoff_len and add_eos_token):
             result["input_ids"].append(tokenizer.eos_token_id)
             result["attention_mask"].append(1)
 
@@ -123,8 +120,7 @@ def train(
             tokenized_full_prompt["labels"] = [
                 -100
             ] * user_prompt_len + tokenized_full_prompt["labels"][
-                user_prompt_len:
-            ]  # could be sped up, probably
+                user_prompt_len:]  # could be sped up, probably
         return tokenized_full_prompt
 
     model = prepare_model_for_int8_training(model)
@@ -141,9 +137,8 @@ def train(
 
     if resume_from_checkpoint:
         # Check the available weights and load them
-        checkpoint_name = os.path.join(
-            resume_from_checkpoint, "pytorch_model.bin"
-        )  # Full checkpoint
+        checkpoint_name = os.path.join(resume_from_checkpoint,
+                                       "pytorch_model.bin")  # Full checkpoint
         if not os.path.exists(checkpoint_name):
             checkpoint_name = os.path.join(
                 resume_from_checkpoint, "adapter_model.bin"
@@ -157,14 +152,17 @@ def train(
         else:
             print(f"Checkpoint {checkpoint_name} not found")
 
-    model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
+    model.print_trainable_parameters(
+    )  # Be more transparent about the % of trainable params.
 
     if val_set_size > 0:
-        train_val = data["train"].train_test_split(
-            test_size=val_set_size, shuffle=True, seed=42
-        )
-        train_data = train_val["train"].shuffle().map(generate_and_tokenize_prompt)
-        val_data = train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+        train_val = data["train"].train_test_split(test_size=val_set_size,
+                                                   shuffle=True,
+                                                   seed=42)
+        train_data = train_val["train"].shuffle().map(
+            generate_and_tokenize_prompt)
+        val_data = train_val["test"].shuffle().map(
+            generate_and_tokenize_prompt)
     else:
         train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
         val_data = None
@@ -191,16 +189,16 @@ def train(
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
         ),
-        data_collator=transformers.DataCollatorForSeq2Seq(
-            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
-        ),
+        data_collator=transformers.DataCollatorForSeq2Seq(tokenizer,
+                                                          pad_to_multiple_of=8,
+                                                          return_tensors="pt",
+                                                          padding=True),
     )
     model.config.use_cache = False
 
     old_state_dict = model.state_dict
-    model.state_dict = (
-        lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
-    ).__get__(model, type(model))
+    model.state_dict = (lambda self, *_, **__: get_peft_model_state_dict(
+        self, old_state_dict())).__get__(model, type(model))
 
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
@@ -209,7 +207,9 @@ def train(
 
     model.save_pretrained(output_dir)
 
-    print("\n If there's a warning about missing keys above, please disregard :)")
+    print(
+        "\n If there's a warning about missing keys above, please disregard :)"
+    )
 
 
 def generate_prompt(data_point):
